fae04f74ca3ae88e7b7379de81119892fa3b0591
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2018-03-03 19:34:33 +0100

    add register component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '../components/Home.vue'
+import Register from '../components/Register.vue'
 import NotesEdit from '../components/Notes.edit'
 import NotesShow from '../components/Notes.show'
 
@@ -11,7 +12,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
+      name: 'home',
       component: Home,
       children: [
         {
@@ -30,6 +31,10 @@ export default new Router({
           props: true
         }
       ]
+    }, {
+      path: '/register',
+      name: 'register',
+      component: Register,
     }
   ]
 })
